3fb7507fd4bbbf4adcb5e1fe9f42d57c6d8a896c
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-26 08:32:57 +0530

    Design Fixed

--- script.js
+++ script.js
@@ -13,24 +13,24 @@ let songIndex = 0;
 let audioElement = new Audio('./songs/naar.mp3');
 let songsList = document.getElementById("songsList");
 let masterPlay = document.getElementById("bottomPlayBtn");
-let myProgressBar = document.getElementById("myProgressBar")
-
-
+let myProgressBar = document.getElementById("myProgressBar");
 
 songs.map((data) => {
     return (
         songsList.innerHTML +=  `<div id="songElem">
             <img src=${data.coverPath} alt=${data.name} />
             <div id="content">
-                <p style="font-weight: bold;">Song: ${data.name}</p>
                 <audio controls class="audio" id="songAudioPlayer">
                     <source src=${data.songsPath} type="audio/ogg" />
                 </audio>
+                <p style="font-weight: bold; margin-top: 15px;">Song: ${data.name}</p>
+                <div id="songInfoContent">
                 <button id="songInfo">Song Info</button>
                 <div style="margin-top: 15px; display: none;" id="songInfoContainer">
                     <h6 style="margin-top: 10px;">Singer Name: Real Boss</h6>
                     <h6 style="margin-top: 10px;">Song Name: Naar</h6>
                     <h6 style="margin-top: 10px;">Duration: 2:59 minutes</h6>
+                </div>
                 </div>
             </div>
         </div>`

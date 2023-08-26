57151780eddfefef9e1b7964f1501928bd96318e
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-26 10:11:28 +0530

    Audio Player

--- script.js
+++ script.js
@@ -5,45 +5,55 @@ let songs = [
         duration: "2:59",
         songsPath: "./songs/naar.mp3",
         coverPath: "https://i.ytimg.com/vi/qfiuMxB9UNU/maxresdefault.jpg"
+    },
+    {
+        id: 2,
+        name: "Toota Jo Kabhi Tara",
+        duration: "5:04",
+        songsPath: "./songs/tara.mp3",
+        coverPath: "https://www.mymp3track.com/wp-content/uploads/2021/07/1625151096529-1536x805.jpg"
     }
-]
+    // Add more songs as needed
+];
 
-// Get Elements
 let songIndex = 0;
-let audioElement = new Audio('./songs/naar.mp3');
+let audioElement = new Audio(songs[songIndex].songsPath);
 let songsList = document.getElementById("songsList");
 let masterPlay = document.getElementById("bottomPlayBtn");
 let myProgressBar = document.getElementById("myProgressBar");
 
 songs.map((data) => {
-    return (
-        songsList.innerHTML +=  `<div id="songElem">
-            <img src=${data.coverPath} alt=${data.name} />
-            <div id="content">
-                <audio controls class="audio" id="songAudioPlayer">
-                    <source src=${data.songsPath} type="audio/ogg" />
-                </audio>
+    songsList.innerHTML += `<div id="songElem">
+        <img src=${data.coverPath} alt=${data.name} />
+        <div id="content">
+            <audio controls class="audio songAudioPlayer" id="songAudioPlayer">
+                <source src=${data.songsPath} type="audio/mpeg" />
+            </audio>
+            <div id="contentCover">
                 <p style="font-weight: bold; margin-top: 15px;">Song: ${data.name}</p>
                 <div id="songInfoContent">
-                <button id="songInfo">Song Info</button>
-                <div style="margin-top: 15px; display: none;" id="songInfoContainer">
-                    <h6 style="margin-top: 10px;">Singer Name: Real Boss</h6>
-                    <h6 style="margin-top: 10px;">Song Name: Naar</h6>
-                    <h6 style="margin-top: 10px;">Duration: 2:59 minutes</h6>
-                </div>
+                    <button class="songInfo">Song Info</button>
+                    <div style="margin-top: 15px; display: none;" class="songInfoContainer">
+                        <h6 style="margin-top: 10px;">Singer Name: Real Boss</h6>
+                        <h6 style="margin-top: 10px;">Song Name: Naar</h6>
+                        <h6 style="margin-top: 10px;">Duration: 2:59 minutes</h6>
+                    </div>
                 </div>
             </div>
-        </div>`
-    )
+        </div>
+    </div>`;
 });
 
+let songAudioPlayer = document.querySelectorAll("#songAudioPlayer");
 
-let songAudioPlayer = document.getElementById("songAudioPlayer");
+songAudioPlayer.forEach((player) => {
+    player.addEventListener("playing", () => {
+        player.style.width = "100%";
+    });
+    
+    player.addEventListener("pause", () => {
+        player.style.width = "20%";
+    });
+})
 
-songAudioPlayer.addEventListener("playing", () => {
-    songAudioPlayer.style.width = "100%";
-});
 
-songAudioPlayer.addEventListener("pause", () => {
-    songAudioPlayer.style.width = "20%";
-});

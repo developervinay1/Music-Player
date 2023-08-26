e464f64579d28da93337a657a22e6cd11ffdc89d
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-26 08:01:49 +0530

    Audio Player Working

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 let songs = [
     {
         id: 1,
-        name: "Sufna Banke",
-        duration: "2:59"
-    },
-    {
-        id: 1,
-        name: "Sufna Banke",
-        duration: "2:59"
-    },
-    {
-        id: 1,
-        name: "Sufna Banke",
-        duration: "2:59"
+        name: "Naar",
+        duration: "2:59",
+        songsPath: "./songs/naar.mp3",
+        coverPath: "https://i.ytimg.com/vi/qfiuMxB9UNU/maxresdefault.jpg"
     }
 ]
 
@@ -27,11 +19,18 @@ let myProgressBar = document.getElementById("myProgressBar")
 songs.map((data) => {
     return (
         songsList.innerHTML +=  `<div id="songElem">
-            <img src="https://c.saavncdn.com/040/Sufna-Banke-Punjabi-2021-20211016000424-500x500.jpg" alt="" />
+            <img src=${data.coverPath} alt=${data.name} />
             <div id="content">
-                <p style="font-weight: bold;">${data.name}</p>
-                <p style="margin-top: 10px;">Duration: ${data.duration}</p>
-                <button>Play Now</button>
+                <p style="font-weight: bold;">Song: ${data.name}</p>
+                <audio controls class="audio">
+                    <source src=${data.songsPath} type="audio/ogg" />
+                </audio>
+                <button id="songInfo">Song Info</button>
+                <div style="margin-top: 15px; display: none;" id="songInfoContainer">
+                    <h6 style="margin-top: 10px;">Singer Name: Real Boss</h6>
+                    <h6 style="margin-top: 10px;">Song Name: Naar</h6>
+                    <h6 style="margin-top: 10px;">Duration: 2:59 minutes</h6>
+                </div>
             </div>
         </div>`
     )
